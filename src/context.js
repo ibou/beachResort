7379f2c95fa6e97cfcacd240430ee4af2dabd587
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-07-12 03:24:27 +0200

    delete content

--- src/context.js
+++ src/context.js
@@ -21,33 +21,33 @@ export default class RoomProvider extends Component {
         breakfast: false,
         pets: false
     };
-    
-    getData = async () => {
-        try {
-            let response = await Client.getEntries({
-                content_type: "beachResortRoom"
-            });
 
-            let rooms = this.formatData(response.items);
+    // getData = async () => {
+    //     try {
+    //         let response = await Client.getEntries({
+    //             content_type: "beachResortRoom"
+    //         });
 
-            let featuredRooms = rooms.filter(room => room.featured === true);
-            //
-            let maxPrice = Math.max(...rooms.map(item => item.price));
-            let maxSize = Math.max(...rooms.map(item => item.size));
-            this.setState({
-                rooms,
-                featuredRooms,
-                sortedRooms: rooms,
-                loading: false,
-                //
-                price: maxPrice,
-                maxPrice,
-                maxSize
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    };
+    //         let rooms = this.formatData(response.items);
+
+    //         let featuredRooms = rooms.filter(room => room.featured === true);
+    //         //
+    //         let maxPrice = Math.max(...rooms.map(item => item.price));
+    //         let maxSize = Math.max(...rooms.map(item => item.size));
+    //         this.setState({
+    //             rooms,
+    //             featuredRooms,
+    //             sortedRooms: rooms,
+    //             loading: false,
+    //             //
+    //             price: maxPrice,
+    //             maxPrice,
+    //             maxSize
+    //         });
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
 
     componentDidMount() {
         // this.getData();
